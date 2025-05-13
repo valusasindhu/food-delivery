3378d29cbe34430e2cb8c6bdca8a30e8bdeca825
valusasindhu <valusasindhu@gmail.com>
2025-05-13 17:07:42 +0530

    admin panel created

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 
 // Add food item
 const addFood = async (req, res) => {
+
     let image_filename = `${req.file.filename}`; // ✅ Fixed
 
     const food = new foodModel({
@@ -22,4 +23,31 @@ const addFood = async (req, res) => {
     }
 };
 
-export { addFood };
+
+//All food list
+const listFood = async (req, res) => {
+    try {
+        const foods = await foodModel.find({});
+        res.json({ success: true, data: foods })
+    }
+    catch (error) {
+        console.log({ success: false, message: "Error" })
+    }
+}
+
+//remove food item
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () => { })
+
+        await foodModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: "Food Removed" })
+    }
+    catch (error) {
+        console.log(error)
+        res.json({ success: false, message: "Error" })
+    }
+}
+
+export { addFood, listFood, removeFood };
