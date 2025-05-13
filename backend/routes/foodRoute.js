2c77f29e406e93d1d17524cb9528c87097b2ad41
valusasindhu <valusasindhu@gmail.com>
2025-05-13 20:43:24 +0530

    login, registration functionality is done

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { addFood, listFood, removeFood } from '../controllers/foodController.js'
 import multer from 'multer'
-
+import { registerUser } from '../controllers/userController.js';
 const foodRouter = express.Router();
 
 //Image Storage Engine
